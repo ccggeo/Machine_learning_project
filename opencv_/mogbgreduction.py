8f927017843d1eedc1538534c6d19d407c2ea41f
ccggeo <georgeconnorc@gmail.com>
2018-01-06 22:07:41 +0000

    Commit

---
+++ opencv_/mogbgreduction.py
@@ -0,0 +1,35 @@
+#detect changes in motion
+
+import cv2
+import numpy as np
+
+cap = cv2.VideoCapture(')
+
+fgbg = cv2.createBackgroundSubtractorMOG2()
+
+while(1):
+    ret, frame = cap.read()
+
+    fgmask = fgbg.apply(frame)
+ 
+    cv2.imshow('fgmask',frame)
+    cv2.imshow('frame',fgmask)
+
+    
+    k = cv2.waitKey(30) & 0xff
+    if k == 27:
+        break
+    
+
+cap.release()
+cv2.destroyAllWindows()
+
+
+
+
+
+
+
+
+
+
